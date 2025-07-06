e2dae42a42d49a054d4a5d53d338f8816f7623e3
Abdulrazak Idris <Abdulrazakidris93@gmail.com>
2025-07-06 15:29:33 +0100

    Create quotes array, and new quote input

---
+++ script.js
@@ -0,0 +1,30 @@
+const quoteDisplay = document.querySelector("#quoteDisplay");
+const newQuoteBtn = document.querySelector("#newQuote");
+
+
+const quotes = [
+    {
+        text: "What a man can do a woman can do better",
+        category: "Local"
+    },
+
+    {
+        text: "Nothing good comes easy",
+        category: "Global"
+    },
+
+    {
+        text: "A journey of a thousand miles, begins with a single step",
+        category: "Global"
+    },
+
+    {
+        text: "Fear woman wey like you, only if you have owo!!",
+        category: "Local"
+    }
+]
+
+function addQuote() {
+   
+}
+
