a2602e3569ba4cb6afad02f55700930e07ecde6a
Abdulrazak Idris <Abdulrazakidris93@gmail.com>
2025-07-06 17:54:50 +0100

    Added Display function

--- dom-manipulation/script.js
+++ dom-manipulation/script.js
@@ -4,22 +4,22 @@ const newQuoteBtn = document.querySelector("#newQuote");
 
 const quotes = [
     {
-        text: "What a man can do a woman can do better",
+        text: "What a man can do a woman can do better.",
         category: "Local"
     },
 
     {
-        text: "Nothing good comes easy",
+        text: "Nothing good comes easy.",
         category: "Global"
     },
 
     {
-        text: "A journey of a thousand miles, begins with a single step",
+        text: "A journey of a thousand miles, begins with a single step.",
         category: "Global"
     },
 
     {
-        text: "Fear woman wey like you, only if you have owo!!",
+        text: "Fear woman wey like you, only if you have owo!!.",
         category: "Local"
     }
 ]
@@ -27,4 +27,34 @@ const quotes = [
 function addQuote() {
    
 }
+
+
+function showRandomQuote() {
+    const randomNumber = Math.floor(Math.random() * (quotes.length));
+    const selectedQuote = quotes[randomNumber];
+    return selectedQuote;
+}
+
+
+function displayRandomQuote(itemObject) {
+    const newQuoteContainer = document.createElement("div");
+    const newQuoteText = document.createElement("p");
+    const newQuoteCategory = document.createElement("p");
+
+    newQuoteContainer.classList.add("newQuoteContainer");
+    newQuoteText.classList.add("text")
+    newQuoteCategory.classList.add("category")
+    newQuoteText.textContent = `QUOTE : ${itemObject.text}`;
+    newQuoteCategory.textContent = `CATEGORY : ${itemObject.category}`;
+
+    // Add to container.
+    newQuoteContainer.appendChild(newQuoteText);
+    newQuoteContainer.appendChild(newQuoteCategory);
+    quoteDisplay.appendChild(newQuoteContainer);
+}
+
+newQuoteBtn.addEventListener("click", () => {
+    const quote  = showRandomQuote();
+    displayRandomQuote(quote);
+})
 
